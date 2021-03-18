396ad431f5c1090f003c3f8cac960d6605f8ace2
fad1105 <fenildomadiya115@gmail.com>
2021-03-18 10:15:45 +0530

    movie table and city table

--- PHPProject_CE029_CE032_CE041/MTB/app/Http/Middleware/Isbranch.php
+++ PHPProject_CE029_CE032_CE041/MTB/app/Http/Middleware/Isbranch.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class Isbranch
+class IsBranch
 {
     /**
      * Handle an incoming request.
@@ -16,19 +16,19 @@ class Isbranch
      */
     public function handle(Request $request, Closure $next)
     {
-        //return $next($request);
+       // return $next($request);
         if(auth()->user()->is_branch == 1){
             return $next($request);
         }
         else {
             if(auth()->user()->is_admin == 1){
                 //return $next($request);
-                return redirect('admin/home')->with('error',"You don't have admin access.");
+                return redirect('admin/home')->with('errors',"You don't have branch access.");
             }
             else {
-                return redirect('home')->with('error',"You don't have admin access.");
+                return redirect('home')->with('errors',"You don't have branch access.");
             }
-        }
+        } 
    
         //return ("error You don't have admin access.");
 
